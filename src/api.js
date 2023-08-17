819c70bd220a1a6d0a601a2af184bc7268308d31
Ujjaval Gupta <ujjavalgupta@Vishals-MBP-2.mshome.net>
2023-08-17 18:03:44 +0530

    Add new Project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '981785ea91msh9e0c614b7da77e6p138ef2jsn0954c7fb0b14',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2c854305c17e0ddd68e341a063176261";
